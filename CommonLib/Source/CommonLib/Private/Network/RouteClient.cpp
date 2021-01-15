973a6515eb2cef0063d22f81258c9693c36dd38f
Dong Chen <vgvgvvvdc@gmail.com>
2021-01-15 18:28:08 +0800

    更新RouteClient逻辑

---
+++ CommonLib/Source/CommonLib/Private/Network/RouteClient.cpp
@@ -0,0 +1,132 @@
+﻿#include "Network/RouteClient.h"
+
+
+
+#include "IPAddress.h"
+#include "IPv4Address.h"
+#include "RunnableThread.h"
+#include "Sockets.h"
+#include "SocketSubsystem.h"
+#include "TcpSocketBuilder.h"
+
+
+FRouteClient::FRouteClient(FString name)
+{
+	Name = name;
+	ShouldStop = false;
+}
+
+
+FRouteClient::~FRouteClient()
+{
+	delete ClientSocket;
+	ClientSocket = nullptr;
+
+	delete RunnableThread;
+	RunnableThread = nullptr;
+}
+
+FRouteClient FRouteClient::Create(FString& name)
+{
+	return FRouteClient(name);
+}
+
+FRouteClient& FRouteClient::SetBuffer(int32 bufferSize)
+{
+	BufferSize = bufferSize;
+	return *this;
+}
+
+FRouteClient& FRouteClient::SetConnectTarget(FString& host, int32 port)
+{
+	FIPv4Address ip;
+	FIPv4Address::Parse(host, ip);
+
+	InternetAddress = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateInternetAddr();
+	InternetAddress->SetIp(ip.Value);
+	InternetAddress->SetPort(port);
+	return *this;
+}
+
+void FRouteClient::Run()
+{
+	ShouldStop = false;
+	while (!ShouldStop)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Connecting To %s ..."), *InternetAddress->ToString(true))
+		if(!Connect())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Connect To %s Error..."), *InternetAddress->ToString(true))
+		}else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Connect To %s Error Success!!"), *InternetAddress->ToString(true))
+			while(ClientSocket != nullptr && ClientSocket->GetConnectionState() == SCS_Connected)
+			{
+
+				WriteMessageBuffer();
+				ReadSocketBuffer();
+				DoHeartBeat();
+			
+				FPlatformProcess::Sleep(0.3);
+			}
+		}
+		FPlatformProcess::Sleep(1);
+	}
+
+	ClientSocket->Close();
+}
+
+void FRouteClient::SendMessage()
+{
+}
+
+
+void FRouteClient::Stop()
+{
+	ShouldStop = true;
+}
+
+bool FRouteClient::Connect()
+{
+
+	if(ClientSocket)
+	{
+		ClientSocket->Close();
+	}
+
+	ClientSocket = ISocketSubsystem::Get(PLATFORM_SOCKETSUBSYSTEM)->CreateSocket(NAME_Stream, host, false);
+
+	if(ClientSocket->Connect(*InternetAddress) == false)
+	{
+		return false;
+	}
+	return true;
+}
+
+void FRouteClient::WriteMessageBuffer()
+{
+}
+
+void FRouteClient::ReadSocketBuffer()
+{
+}
+
+void FRouteClient::DoHeartBeat()
+{
+}
+
+bool FRouteClientRunner::Init()
+{
+	return true;
+}
+
+uint32 FRouteClientRunner::Run()
+{
+	Client.Run();
+	return true;
+}
+
+void FRouteClientRunner::Stop()
+{
+	return;
+}
---
+++ CommonLib/Source/CommonLib/Public/Network/RouteClient.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+#include "IPv4Endpoint.h"
+#include "Runnable.h"
+#include "CoreMinimal.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnRegistRouteClient);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnCloseRouteClient);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHeartBeatRouteClient);
+
+class FRouteClient
+{
+public:
+	//Client属性相关
+
+	FRouteClient& SetBuffer(int32 bufferSize);
+	FRouteClient& SetConnectTarget(FString& host, int32 port);
+
+	FOnRegistRouteClient OnRegist;
+	FOnCloseRouteClient OnClose;
+	FOnHeartBeatRouteClient OnHeartBeat;
+
+private:
+
+	TSharedRef<FInternetAddr> InternetAddress;
+	int32 BufferSize = 2 * 1024 * 1024;	
+	FString Name;
+
+
+public:
+	//生命周期相关
+	
+	static FRouteClient Create(FString& name);
+	void Run();
+	void SendMessage();
+	void Stop();
+private:
+
+	bool Connect();
+
+	void WriteMessageBuffer();
+	void ReadSocketBuffer();
+	void DoHeartBeat();
+
+private:
+
+	FRouteClient(FString name);
+	~FRouteClient();
+	
+	FSocket* ClientSocket = nullptr;
+	FRunnableThread* RunnableThread = nullptr;
+	
+
+	bool ShouldStop = false;
+};
+
+class FRouteClientRunner : public FRunnable
+{
+public:
+
+	FRouteClientRunner(FRouteClient& client) : Client(client){} 
+	
+	virtual bool Init() override;
+	virtual uint32 Run() override;
+	virtual void Stop() override;
+private:
+	FRouteClient& Client;
+};
