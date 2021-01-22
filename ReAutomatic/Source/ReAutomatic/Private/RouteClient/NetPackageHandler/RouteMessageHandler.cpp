8ee6759e20009a87c9b0de44483cadd72caa26a5
Dong Chen <vgvgvvvdc@gmail.com>
2021-01-22 10:26:49 +0800

    更新

--- ReAutomatic/Source/ReAutomatic/Private/RouteClient/NetPackageHandler/RouteMessageHandler.cpp
+++ ReAutomatic/Source/ReAutomatic/Private/RouteClient/NetPackageHandler/RouteMessageHandler.cpp
@@ -21,24 +21,24 @@ void FRouteMessageHandler::HandleJsonInfo(FSocketClient& from, TSharedPtr<FJsonO
 		*FJsonUtil::JsonObjectToString(jsonObject))
 
 	auto ContentObject = jsonObject->GetObjectField("Content");
+	// RawCommand
 	auto CmdId = ContentObject->GetStringField("CmdId");
+	auto RawCommandContent = ContentObject->GetField<EJson::None>("Content");
 
 	if(CmdId == FCmdTypes::Log)
 	{
-		auto Value = ContentObject->GetField<EJson::String>("Content");
-		auto LogStr = Value->AsString();
+		auto LogStr = RawCommandContent->AsString();
 		UE_LOG(LogAutomatic, Log, TEXT("Log From Route Server : %s"), *LogStr)
 	}
 	else if (CmdId == FCmdTypes::UE4Msg)
 	{
-		auto Value = ContentObject->GetField<EJson::Object>("Content");
-		auto UE4JsonObj = Value->AsObject();
-
+		auto UE4MessageObject = RawCommandContent->AsObject();
 		
-		auto CmdId = UE4JsonObj->GetStringField("CmdId");
-		auto CmdContent = UE4JsonObj->GetField<EJson::None>("Content");
+		auto CmdId = UE4MessageObject->GetStringField("CmdId");
+		auto Index = UE4MessageObject->GetIntegerField("Index");
+		auto CmdContent = UE4MessageObject->GetField<EJson::None>("Content");
 
-		auto Arg = FUE4CmdArg(CmdId, CmdContent);
+		auto Arg = FUE4CmdArg(CmdId, RawCommandContent);
 
 		AsyncTask(ENamedThreads::GameThread, [CmdId, Arg]()
 		{
@@ -48,8 +48,9 @@ void FRouteMessageHandler::HandleJsonInfo(FSocketClient& from, TSharedPtr<FJsonO
 	}
 	else if (CmdId == FCmdTypes::RunLua)
 	{
-		auto Value = ContentObject->GetField<EJson::String>("Content");
-		auto LuaStr = Value->AsString();
+		auto RunLuaMessageObject = RawCommandContent->AsObject();
+		auto Index = RunLuaMessageObject->GetIntegerField("Index");
+		auto LuaStr = RunLuaMessageObject->GetStringField("LuaScript");
 
 		AsyncTask(ENamedThreads::GameThread, [LuaStr]()
 		{
