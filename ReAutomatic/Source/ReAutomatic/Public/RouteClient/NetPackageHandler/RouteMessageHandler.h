07821bb4495fa26a3364a4154b09171dccab97aa
dieseldong <dieseldong@tencent.com>
2021-03-11 14:57:12 +0800

    更新

--- ReAutomatic/Source/ReAutomatic/Public/RouteClient/NetPackageHandler/RouteMessageHandler.h
+++ ReAutomatic/Source/ReAutomatic/Public/RouteClient/NetPackageHandler/RouteMessageHandler.h
@@ -1,10 +1,16 @@
 #pragma once
 #include "JsonPackageHandler.h"
 
+enum DownloadResult;
+
 class FRouteMessageHandler : public FJsonPackageHandler
 {
 public:
 
 	virtual void HandleJsonInfo(FSocketClient& from, TSharedPtr<FJsonObject> jsonObject) override;
-	
+
+private:
+
+	void OnDownloadFileProgress(const int32 BytesSent, const int32 BytesReceived, const int32 ContentLength);
+	void OnDownloadFileResult(TEnumAsByte<DownloadResult> Result);
 };
