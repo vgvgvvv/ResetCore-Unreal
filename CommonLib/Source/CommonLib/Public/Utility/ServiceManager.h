03725d22912b380a86e98d4a2645b77979b2662c
dieseldong <dieseldong@tencent.com>
2021-03-25 17:30:16 +0800

    增加Service管理器

---
+++ CommonLib/Source/CommonLib/Public/Utility/ServiceManager.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "ServiceManager.generated.h"
+
+USTRUCT()
+struct FServiceCollection
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY()
+	TMap<FString, UObject*> ServiceMap;
+};
+
+UCLASS()
+class COMMONLIB_API UServiceManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+
+	UFUNCTION()
+	void RegisterService(UObject* Service, UClass* ServiceType, const FString& ServiceName = "");
+
+	UFUNCTION()
+	void UnregisterService(UClass* ServiceType, const FString& ServiceName = "");
+	
+	UFUNCTION()
+	UObject* GetService(UClass* ServiceType, const FString& ServiceName = "");
+private:
+	UPROPERTY()
+	TMap<FName, FServiceCollection> Services;
+};
