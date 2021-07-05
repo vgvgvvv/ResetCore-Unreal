31c5b3cff19838b3edff06884e4d572f85dbfe5f
dieseldong <dieseldong@tencent.com>
2021-07-05 17:15:49 +0800

    更新

--- DotNetHoster/Source/DotNetHoster/Public/API/Log.h
+++ DotNetHoster/Source/DotNetHoster/Public/API/Log.h
@@ -1,7 +1,10 @@
 ﻿#pragma once
+#include <string>
 
-class Log
+class DOTNETHOSTER_API FLog
 {
 public:
-	
+	static void Log(const std::string& Info);
+	static void Warn(const std::string& Info);
+	static void Error(const std::string& Info);
 };
